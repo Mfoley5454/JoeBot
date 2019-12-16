d8ca7bc46b07cb91cfba5433e990dc5213c5b9b2
Mfoley5454 <58917244+Mfoley5454@users.noreply.github.com>
2019-12-16 11:35:09 -0600

    Create RedditBot.py

---
+++ RedditBot.py
@@ -0,0 +1,8 @@
+import praw
+
+
+reddit = praw.Reddit(client_id='cbEXEwIUsqosaw',
+                     client_secret='2E3xKDn4h3qg8TjTx86lQEsLnSo',
+                     username = 'joebot69',
+                     password = 'Joeis69dumb',
+                     user_agent = 'joebot69 by /u/Joe')
