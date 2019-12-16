b5872732faf7856c56e704dfd9f1549e34ea1517
jwinfield27 <34040553+jwinfield27@users.noreply.github.com>
2019-12-16 14:01:55 -0600

    cleaning up input
    completed reading posts from writingPrompts
    #TODO finish up cleaning input text

--- RedditBot.py
+++ RedditBot.py
@@ -1,8 +1,28 @@
 import praw
+import re
+from requests import Session
+from praw.models import MoreComments
 
 
-reddit = praw.Reddit(client_id='cbEXEwIUsqosaw',
-                     client_secret='2E3xKDn4h3qg8TjTx86lQEsLnSo',
-                     username = 'joebot69',
-                     password = 'Joeis69dumb',
-                     user_agent = 'joebot69 by /u/Joe')
+subRedditList = ['todayilearned','writingprompts','copypasta','tifu','askReddit']
+writeFile = open("testoutput.txt", "w+")
+
+reddit = praw.Reddit(client_id='AykrCJZQwoCAxA',
+                     client_secret='f8hSfCbHiREbEgSJa_RKYaOIOCk',
+                     username = 'doritodustgamergunk',
+                     password = 'JoeBot2019',
+                     user_agent = 'dumbBot by /u/doritodustgamergunk'
+                     )
+wordCount = 0.0
+for submission in reddit.subreddit("writingprompts").top(limit=5):
+    comments = submission.comments
+    for comment in comments:
+        if isinstance(comment, MoreComments):
+            continue
+        if comment.score > 1000:
+            comment.body = re.sub("\n", "", comment.body)
+            comment.body = comment.body.replace('\"','')
+            comment.body = comment.body.replace('”','')
+            comment.body = comment.body.replace('“','')
+            writeFile.write(comment.body)
+            wordCount += len(comment.body.split(" "))
